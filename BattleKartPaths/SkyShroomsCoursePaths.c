bccc413086f10e00e4181f8a28d7d83ef6db7148
Kyle Kaplan <kfkaplan@astro.as.utexas.edu>
2024-09-24 01:09:03 -0500

    Add skychrooms paths.

---
+++ BattleKartPaths/SkyShroomsCoursePaths.c
@@ -0,0 +1,109 @@
+#include "../../Library/MainInclude.h"
+
+
+
+Marker SkyShroomsPaths_Path11[2] = 
+{
+	{ {-375, 90, 194} , 1},
+	{ {-403, 90, 270} , 1},
+};
+Marker SkyShroomsPaths_Path10[2] = 
+{
+	{ {-375, 90, 375} , 1},
+	{ {-270, 90, 403} , 1},
+};
+Marker SkyShroomsPaths_Path09[8] = 
+{
+	{ {-194, 90, 375} , 1},
+	{ {-130, 116, 375} , 1},
+	{ {-67, 142, 375} , 1},
+	{ {-33, 148, 375} , 1},
+	{ {0, 154, 375} , 1},
+	{ {33, 160, 375} , 1},
+	{ {67, 165, 375} , 1},
+	{ {130, 165, 375} , 1},
+};
+Marker SkyShroomsPaths_Path08[8] = 
+{
+	{ {-375, 165, -194} , 1},
+	{ {-375, 165, -130} , 1},
+	{ {-375, 165, -67} , 1},
+	{ {-375, 160, -33} , 1},
+	{ {-375, 154, 0} , 1},
+	{ {-375, 148, 33} , 1},
+	{ {-375, 142, 67} , 1},
+	{ {-375, 116, 130} , 1},
+};
+Marker SkyShroomsPaths_Path07[2] = 
+{
+	{ {-375, 165, -375} , 1},
+	{ {-403, 165, -270} , 1},
+};
+Marker SkyShroomsPaths_Path06[2] = 
+{
+	{ {-194, 165, -375} , 1},
+	{ {-270, 165, -403} , 1},
+};
+Marker SkyShroomsPaths_Path05[2] = 
+{
+	{ {375, 90, -194} , 1},
+	{ {403, 90, -270} , 1},
+};
+Marker SkyShroomsPaths_Path04[2] = 
+{
+	{ {375, 90, -375} , 1},
+	{ {270, 90, -403} , 1},
+};
+Marker SkyShroomsPaths_Path03[8] = 
+{
+	{ {194, 90, -375} , 1},
+	{ {130, 116, -375} , 1},
+	{ {67, 142, -375} , 1},
+	{ {33, 148, -375} , 1},
+	{ {0, 154, -375} , 1},
+	{ {-33, 160, -375} , 1},
+	{ {-67, 165, -375} , 1},
+	{ {-130, 165, -375} , 1},
+};
+Marker SkyShroomsPaths_Path02[8] = 
+{
+	{ {375, 165, 194} , 1},
+	{ {375, 165, 130} , 1},
+	{ {375, 165, 67} , 1},
+	{ {375, 160, 33} , 1},
+	{ {375, 154, 0} , 1},
+	{ {375, 148, -33} , 1},
+	{ {375, 142, -67} , 1},
+	{ {375, 116, -130} , 1},
+};
+Marker SkyShroomsPaths_Path01[2] = 
+{
+	{ {375, 165, 375} , 1},
+	{ {403, 165, 270} , 1},
+};
+Marker SkyShroomsPaths_Path00[2] = 
+{
+	{ {194, 165, 375} , 1},
+	{ {270, 165, 403} , 1},
+};
+
+Marker *SkyShroomsPaths_Paths[] = 
+{ SkyShroomsPaths_Path11, SkyShroomsPaths_Path10, SkyShroomsPaths_Path09, SkyShroomsPaths_Path08, SkyShroomsPaths_Path07, SkyShroomsPaths_Path06, SkyShroomsPaths_Path05, SkyShroomsPaths_Path04, SkyShroomsPaths_Path03, SkyShroomsPaths_Path02, SkyShroomsPaths_Path01, SkyShroomsPaths_Path00};
+
+short SkyShroomsPaths_PathLengths[12] = 
+{ 1, 1, 7, 7, 1, 1, 1, 1, 7, 7, 1, 1};
+
+Marker *SkyShroomsPaths_Ramps[] = 
+{ };
+short SkyShroomsPaths_RampLengths[0] = 
+{};
+
+
+
+
+Marker *SkyShroomsPaths_Drops[] = 
+{ };
+short SkyShroomsPaths_DropLengths[0] = 
+{ };
+short SkyShroomsPaths_LineCounts[3] = 
+{ 12, 0, 0};
---
+++ BattleKartPaths/SkyShroomsCoursePaths.h
@@ -0,0 +1,22 @@
+#include "../../Library/MainInclude.h"
+
+extern Marker *SkyShroomsPaths_Paths[12];
+extern short SkyShroomsPaths_PathLengths[12];
+extern Marker SkyShroomsPaths_Path11[2];
+extern Marker SkyShroomsPaths_Path10[2];
+extern Marker SkyShroomsPaths_Path09[8];
+extern Marker SkyShroomsPaths_Path08[8];
+extern Marker SkyShroomsPaths_Path07[2];
+extern Marker SkyShroomsPaths_Path06[2];
+extern Marker SkyShroomsPaths_Path05[2];
+extern Marker SkyShroomsPaths_Path04[2];
+extern Marker SkyShroomsPaths_Path03[8];
+extern Marker SkyShroomsPaths_Path02[8];
+extern Marker SkyShroomsPaths_Path01[2];
+extern Marker SkyShroomsPaths_Path00[2];
+extern Marker *SkyShroomsPaths_Ramps[0];
+extern short SkyShroomsPaths_RampLengths[0];
+extern Marker *SkyShroomsPaths_Drops[0];
+extern short SkyShroomsPaths_DropLengths[0];
+
+extern short SkyShroomsPaths_LineCounts[3];
